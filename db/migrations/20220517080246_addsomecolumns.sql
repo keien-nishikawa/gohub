afa1223a6e0657e63181aa30fc647cc03ba2ea97
keien-nishikawa <keien.nishikawa@clubx.jp>
2022-05-17 17:56:34 +0900

    wip:commit

--- db/migrations/20220517080246_addsomecolumns.sql
+++ db/migrations/20220517080246_addsomecolumns.sql
@@ -1,9 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
+CREATE TABLE gohub_user (
+  id int,
+  last_name varchar(30),
+  first_name varchar(30),
+  age int
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
+DROP TABLE t_user
 -- +goose StatementEnd
