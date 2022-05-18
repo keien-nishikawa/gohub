49ec4d40b0b9f2175e64d116b87d5e6ea166facb
keien-nishikawa <keien.nishikawa@clubx.jp>
2022-05-18 19:54:39 +0900

    ログ対応

---
+++ cmd/util/logo.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+// log設定をする関数
+func LoggingSettings(filename string) {
+	logfile, _ := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.SetOutput(multiLogFile)
+}
--- server.go
+++ server.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	router "gohub/cmd/config"
+	"gohub/cmd/util"
 	"log"
 	"net/http"
 )
 
 func main() {
 	router.SetRouter()
-	err := http.ListenAndServe(":8080",nil)
+ 	err := http.ListenAndServe(":8080",nil)
+	util.LoggingSettings("develop.log")
+	log.Println("test start.")
 	if err != nil {
 		log.Fatal(err)
 	}
